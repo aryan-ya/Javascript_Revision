6c160bc01c590b3c9497635cb4dec5a68ece9b06
Aryan Kumar Yadav <ay3978943@gmail.com>
2024-02-05 11:54:16 +0530

    Initial commit

---
+++ classs2.js/class2.js
@@ -0,0 +1,47 @@
+
+
+// Arithmetic operator
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b =" , a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =" , a ** b);
+
+
+// Unary operator
+
+// let a = 5 ;
+// let b = 2 ;
+
+// console.log("a =", a,  " & b = " , b  );
+
+// console.log("a++ =" , a++);
+// console.log(a);
+
+
+// console.log("--a" , a--);
+// console.log(a);
+
+
+// ASSIGNMENT OPERATOR
+
+let a = 5 ;
+let b = 10;
+
+// a += 4
+
+//a= a+4
+
+
+// a /= 4;
+
+// a%= 4;
+
+a **= 4 ; 
+console.log("a =" , a);
+
