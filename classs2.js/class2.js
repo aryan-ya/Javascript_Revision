9b9299a8c2224ad2f52e989a4c4d3a07f9c0f9b5
Aryan Kumar Yadav <ay3978943@gmail.com>
2024-02-05 16:15:32 +0530

    class 2

--- classs2.js/class2.js
+++ classs2.js/class2.js
@@ -30,8 +30,8 @@
 
 // ASSIGNMENT OPERATOR
 
-let a = 5 ;
-let b = 10;
+// let a = 5 ;
+// let b = 10;
 
 // a += 4
 
@@ -42,6 +42,170 @@ let b = 10;
 
 // a%= 4;
 
-a **= 4 ; 
-console.log("a =" , a);
+// a **= 4 ; 
+// console.log("a =" , a);
 
+
+
+
+// Comparison Operator
+
+// let a = 5 ;
+// let b = 3 ;
+
+// console.log("a == b" , a == b );
+
+// console.log("5 != 2", a !=b);
+
+
+
+//logical operator
+
+// let a = 6 ;
+// let b= 5;
+
+
+// AND
+
+
+// console.log("cond1 && cond2 =" , a < b   && a === 6);
+
+
+// or
+
+// console.log("cond1 && cond2 =" , a < b   || a === 6);
+
+
+//not
+
+// console.log("!6<5 = ", !(a === 6));
+
+
+
+// Conditional statements
+
+
+// IF STATEMENTS
+
+// let age = 15;
+// if(age > 18){
+//     console.log(" you can vote");
+// }
+
+
+// if (age < 18) {
+//     console.log("you cannot vote");
+// }
+
+
+
+
+// let mode = "dark";
+// let color;
+
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// if (mode === "light") {
+//     color = "white";
+// }
+
+
+// console.log(color);
+
+
+
+// IF ELSE STATEMENTS
+
+
+
+
+// let mode = "dark";
+// let color;
+
+
+// if (mode === "dark") {
+//     color = "black";
+// }else{
+//     color = "white";
+// }
+
+
+
+// console.log(color);
+
+
+// odd or even
+
+// let num = 11;
+
+
+// if (num % 2 === 0) {
+//     console.log(num , "is even");
+// }else{
+//     console.log(num , "is odd");
+// }
+
+
+// else if statements
+
+// if(age < 18){
+//     console.log("juniors");
+// }else if(age > 60){
+//     console.log("senior");
+// }else{
+//     console.log("middle");
+// }
+
+
+
+// conditional statements
+
+// let mode = "blue";
+// let color;
+
+
+// if (mode === "dark") {
+//     color ="black";
+// }else if (mode === "blue") {
+//     color = "blue";
+// }else if (mode === "pink") {
+//     color = "pink";
+// }else{
+//     color = "white";
+// }
+
+
+// console.log(color);
+
+
+// TERNARY OPERATOR
+
+
+// let age = 25;
+
+//  let result= age >=  18 ? "adult" : "not adult";
+// console.log(result);
+
+
+
+// alert("hello");
+
+
+
+
+// let name = prompt("hello");
+
+// console.log(name);
+
+
+
+
+let num = prompt("enter a number");
+if(num % 5 === 0 ){
+    console.log(num, "is  a multiple of 5");
+}else{
+    console.log(num, "is  not  a multiple of 5"); 
+}
